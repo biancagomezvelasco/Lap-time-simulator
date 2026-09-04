293b50a45f6a50580d884336c80d55f9aabdc7a1
biancagomezvelasco <j4gy6dnztn@privaterelay.appleid.com>
2026-09-04 17:06:35 +0100

    Update Corner_speed.py

--- Corner_speed.py
+++ Corner_speed.py
@@ -1,1 +1,8 @@
-
+import numpy as np
+mu = 0.50
+g = 9.81
+r = 80
+vmax_m_per_second = np.sqrt(mu * g * r)
+vmax_km_per_hour = (vmax_m_per_second / 1000) * 60**2
+print vmax_m_per_second ("m/s")
+print vmax_km_per_hour ("km/h")
